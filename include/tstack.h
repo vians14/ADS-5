c95554bde5a92c3d004733405b505910bc7c0b20
vians14 <131287803+vians14@users.noreply.github.com>
2026-04-21 20:24:28 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,46 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+    T data[n];
+    int top;
+    
+public:
+    TStack() : top(-1) {}
+    
+    void push(const T& value) {
+        if (topIndex >= n - 1) {
+            throw std::overflow_error("Stack overflow");
+        }
+        data[++topIndex] = value;
+    }
+    
+    T pop() {
+        if (isEmpty()) {
+            throw std::underflow_error("Stack underflow");
+        }
+        return data[top--];
+    }
+    
+    T top() const {
+        if (isEmpty()) {
+            throw std::underflow_error("Stack is empty");
+        }
+        return data[top];
+    }
+    
+    bool isEmpty() const {
+        return top == -1;
+    }
+    
+    int size() const {
+        return top + 1;
+    }
+    
+    void clear() {
+        top = -1;
+    }
 };
+
 
 #endif  // INCLUDE_TSTACK_H_
