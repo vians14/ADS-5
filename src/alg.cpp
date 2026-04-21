08f4648f90e7632c762ed70fc8dccdd8d0ade115
vians14 <131287803+vians14@users.noreply.github.com>
2026-04-21 20:55:27 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,7 +4,7 @@
 #include "tstack.h"
 #include <cctype>
 
-using namespace std;
+using std::string;
 
 int prior(char op) {
     if (op == '+' || op == '-') return 1;
@@ -15,24 +15,22 @@ int prior(char op) {
 string infx2pstfx(const string& inf) {
     TStack<char, 100> st;
     string out = "";
-    
-    for (int i = 0; i < inf.length(); i++) {
+
+    for (int i = 0; i < static_cast<int>(inf.length()); i++) {
         char ch = inf[i];
-        
+
         if (ch == ' ') continue;
-        
+
         if (isdigit(ch)) {
-            while (i < inf.length() && isdigit(inf[i])) {
+            while (i < static_cast<int>(inf.length()) && isdigit(inf[i])) {
                 out = out + inf[i];
                 i++;
             }
             out = out + ' ';
             i--;
-        }
-        else if (ch == '(') {
+        } else if (ch == '(') {
             st.push(ch);
-        }
-        else if (ch == ')') {
+        } else if (ch == ')') {
             while (!st.empty() && st.top() != '(') {
                 out = out + st.pop();
                 out = out + ' ';
@@ -40,71 +38,71 @@ string infx2pstfx(const string& inf) {
             if (!st.empty() && st.top() == '(') {
                 st.pop();
             }
-        }
-        else {
-            while (!st.empty() && st.top() != '(' && prior(st.top()) >= prior(ch)) {
+        } else {
+            while (!st.empty() && st.top() != '(' &&
+                   prior(st.top()) >= prior(ch)) {
                 out = out + st.pop();
                 out = out + ' ';
             }
             st.push(ch);
         }
     }
-    
+
     while (!st.empty()) {
         out = out + st.pop();
         out = out + ' ';
     }
-    
+
     if (out.length() > 0 && out[out.length() - 1] == ' ') {
         out.pop_back();
     }
-    
+
     return out;
 }
 
 int eval(const string& post) {
     TStack<int, 100> s;
     string tmp = "";
-    
-    for (int i = 0; i < post.size(); i++) {
+
+    for (int i = 0; i < static_cast<int>(post.size()); i++) {
         char c = post[i];
-        
+
         if (isdigit(c)) {
             tmp = tmp + c;
         }
-        
+
         if (c == ' ') {
             if (tmp != "") {
                 int x = 0;
-                for (int j = 0; j < tmp.length(); j++) {
+                for (int j = 0; j < static_cast<int>(tmp.length()); j++) {
                     x = x * 10 + (tmp[j] - '0');
                 }
                 s.push(x);
                 tmp = "";
             }
         }
-        
+
         if (c == '+' || c == '-' || c == '*' || c == '/') {
             int b = s.pop();
             int a = s.pop();
-            
+
             int r;
             if (c == '+') r = a + b;
             if (c == '-') r = a - b;
             if (c == '*') r = a * b;
             if (c == '/') r = a / b;
-            
+
             s.push(r);
         }
     }
-    
+
     if (tmp != "") {
         int x = 0;
-        for (int j = 0; j < tmp.length(); j++) {
+        for (int j = 0; j < static_cast<int>(tmp.length()); j++) {
             x = x * 10 + (tmp[j] - '0');
         }
         s.push(x);
     }
-    
+
     return s.pop();
 }
