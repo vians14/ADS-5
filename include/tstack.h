e73ad96899a6bb5ea715efa3cc1e65e0641d1b57
vians14 <131287803+vians14@users.noreply.github.com>
2026-04-21 20:37:56 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -5,14 +5,14 @@
 template<typename T, int size>
 class TStack {
 private:
-    T data[n];
-    int top;
+    T data[N];
+    int topIndex;
     
 public:
-    TStack() : top(-1) {}
+    TStack() : topIndex(-1) {}
     
     void push(const T& value) {
-        if (topIndex >= n - 1) {
+        if (topIndex >= N - 1) {
             throw std::overflow_error("Stack overflow");
         }
         data[++topIndex] = value;
@@ -22,26 +22,26 @@ public:
         if (isEmpty()) {
             throw std::underflow_error("Stack underflow");
         }
-        return data[top--];
+        return data[topIndex--];
     }
     
     T top() const {
         if (isEmpty()) {
             throw std::underflow_error("Stack is empty");
         }
-        return data[top];
+        return data[topIndex];
     }
     
     bool isEmpty() const {
-        return top == -1;
+        return topIndex == -1;
     }
     
     int size() const {
-        return top + 1;
+        return topIndex + 1;
     }
     
     void clear() {
-        top = -1;
+        topIndex = -1;
     }
 };
 
